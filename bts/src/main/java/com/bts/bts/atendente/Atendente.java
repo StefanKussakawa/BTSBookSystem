a8d6d60380f60804caeb701d449054e12e08cec1
Stefan <stefanck21@gmail.com>
2021-03-16 20:04:25 -0300

    Criado classe Atendente

--- bts/src/main/java/com/bts/bts/atendente/Atendente.java
+++ bts/src/main/java/com/bts/bts/atendente/Atendente.java
@@ -1,5 +1,21 @@
 package com.bts.bts.atendente;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
 public class Atendente {
+
+    @Id
+    @GeneratedValue
+    private Long idAtendente;
+    private String nome;
+    private String senha;
 
 }
